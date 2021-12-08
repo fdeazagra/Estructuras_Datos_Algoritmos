d05d87d8ae90c528fe9c8185a724519ad0bc3f75
Fernando Azagra <73386163+fdeazagra@users.noreply.github.com>
2021-12-08 18:04:27 +0100

    Borrar item de un arreglo

--- src/arreglo/ArregloApp.java
+++ src/arreglo/ArregloApp.java
@@ -6,11 +6,11 @@ public class ArregloApp {
 		
 		long[] arr;
 		arr = new long[20];
-		int numElems;
+		int numElems;					// tamaño del arreglo
 		int j;							// índice del arreglo
 		long buscarItem;				// item a buscar en el arreglo
 		
-		// insertamos 10 items en el arreglo
+		// insertamos 10 items de ejemplo en el arreglo
 		
 		arr[0] = 77;
 		arr[1] = 22;
@@ -25,27 +25,61 @@ public class ArregloApp {
 		numElems = 10;
 		
 		for(j=0; j<numElems; j++) {
+			
 			System.out.print(arr[j] + " ");
 		}
 		System.out.println("");
 		
 		// ALGORITMO DE BÚSQUEDA LINEAL
 		
-		// Buscamos item con valor x en el arreglo
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		// BUSCAR ITEM ejemplo con valor 12
 		
 		buscarItem = 12;
-		for(j=0; j<numElems; j++) {
+		
+		for(j=0; j<numElems; j++) {			// bucle para recorrer el arreglo
+			
 			if(arr[j] == buscarItem) {		// item encontrado
-				break;
+				break;						// salir del bucle for porque se ha encontrado el item
 			}
 		}
 		
-		if(j==numElems) {
+		if(j==numElems) {					// imprimir resultado
+			
 			System.out.println("No se encuentra el número " + buscarItem);
+			
 		} else {
+			
 			System.out.println("Número encontrado " + buscarItem);
 		}
+		
+		//--------------------------------------------------------------------------------------------------------
+		
+		// BORRAR ITEM ejemplo con valor 53
 
+		buscarItem = 53;
+		
+		for(j=0; j<numElems; j++) {			// bucle para recorrer el arreglo
+			
+			if(arr[j] == buscarItem) {		// item encontrado
+				break;						// salir del bucle for porque se ha encontrado el item
+			}
+		}
+		
+		for(int k=j; k<numElems; k++) {		// mover items superiores al item encontrado hacia abajo
+			
+			arr[k] = arr[k+1];				// sobreescribir cada item con su posterior
+		}
+		
+		numElems--;							// reducir en uno el tamaño del arreglo
+		
+		for(j=0;j<numElems;j++) {			// bucle para recorrer el arreglo e imprimir resultado
+			
+			System.out.print(arr[j] + " ");
+		}
+		
+		//--------------------------------------------------------------------------------------------------------
 	}
 
 }
